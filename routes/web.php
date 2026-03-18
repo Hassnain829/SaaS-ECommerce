71712031dfbc3c4cf591db15fc8a10a4e314e16c
Hassnain829 <Edwardsmith8290@gmail.com>
2026-03-19 02:12:58 +0500

    way to creating store

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-
+// 
 Route::get('/', function () {
     return view('welcome');
 });
